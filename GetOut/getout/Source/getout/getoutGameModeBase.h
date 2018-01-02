76b07381a19edb909f3189197cc4becdf24a8f83
JOYCITY\hunja8282 <hunja8282@>
2018-01-02 11:10:48 +0900

    Inital commit

---
+++ GetOut/getout/Source/getout/getoutGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "getoutGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GETOUT_API AgetoutGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
